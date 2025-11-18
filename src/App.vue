68a12ac62271a300475865cef815d238eefa5106
hostimmanu@gmail.com <hostimmanu@gmail.com>
2025-11-18 09:23:53 -0300

    fix: arrumar o layout

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,11 @@
 <script setup>
   import FooterComp from './components/FooterComp.vue';
   import HeaderComp from './components/HeaderComp.vue';
-  
+
 </script>
 
 <template>
-    <HeaderComp />
-
-    <FooterComp />
-<template>
-
+<HeaderComp />
+<FooterComp />
+</template>
 <style scoped></style>
