967c5f6c3a4a8dfec2d81ffc289e09e683f4ca0f
manuhostin <hostimmanu@gmail.com>
2026-06-16 15:18:23 -0300

    resolver conflito

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,30 @@
-<script setup></script>
+<script setup>
+import FooterComp from './components/FooterComp.vue'
+import HeaderComp from './components/HeaderComp.vue'
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div id="app">
+    <HeaderComp />
+    <main class="page-content">
+      <router-view />
+    </main>
+    <FooterComp />
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+#app {
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+}
+
+.page-content {
+  flex: 1;
+  width: 100%;
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 28px 20px;
+}
+</style>
